aff242c6ac365dfb9f3c5d29a106f95eff1d53e6
S Yoon <sohizzhang@gmail.com>
2019-06-01 13:29:08 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz.rb
+++ spec/fizzbuzz.rb
@@ -2,16 +2,4 @@ def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
   end
-
-  elsif int % 5 == 0
-    puts "Buzz"
-  end
-
-  elsif int % 15 == 0
-    puts "FizzBuzz"
-  end
-
-  elsif int % 4 == 0
-    puts "nil"
-  end
 end
